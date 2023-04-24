29915844b255525269fcb8523d1ce48079c037e7
Jesse Bakker <github@jessebakker.com>
2023-04-24 17:58:02 +0200

    DTW

--- align/src/dtw.rs
+++ align/src/dtw.rs
@@ -1,225 +1,351 @@
-use std::cmp::Ordering;
+pub(crate) mod fastdtw;
+mod window;
 
-use ndarray::prelude::*;
+use std::ops::Index;
 
-use crate::dtw_striped::DTWStriped;
+use crate::CmpF32;
 
-pub(crate) enum DtwAlgorithm {
-    Exact(DTWExact),
-    Striped(DTWStriped),
+pub struct DtwResult {
+    pub path: Vec<(usize, usize)>,
+    pub cost: f32,
 }
 
-impl Dtw for DtwAlgorithm {
-    fn path(&self, mfcc1: &Array2<f32>, mfcc2: &Array2<f32>) -> Vec<(usize, usize)> {
-        match self {
-            DtwAlgorithm::Exact(dtw) => dtw.path(mfcc1, mfcc2),
-            DtwAlgorithm::Striped(dtw) => dtw.path(mfcc1, mfcc2),
-        }
+macro_rules! min {
+        ($($args:expr),*) => {{
+        let result = f32::INFINITY;
+        $(
+            let result = result.min($args);
+        )*
+        result
+    }}
+}
+
+pub(crate) struct SparseCostMatrix {
+    indices: Vec<usize>,
+    cost_matrix: Vec<f32>,
+    windows: Vec<(usize, usize)>,
+    size: (usize, usize),
+}
+
+impl SparseCostMatrix {
+    fn size(&self) -> (usize, usize) {
+        self.size
+    }
+
+    fn materialize(&self) -> Vec<Vec<f32>> {
+        (0..self.size().0)
+            .map(|i| (0..self.size().1).map(|j| self[(i, j)]).collect())
+            .collect()
     }
 }
 
-pub(crate) trait Dtw {
-    fn path(&self, mfcc1: &Array2<f32>, mfcc2: &Array2<f32>) -> Vec<(usize, usize)>;
+impl Index<(usize, usize)> for SparseCostMatrix {
+    type Output = f32;
+
+    fn index(&self, index: (usize, usize)) -> &Self::Output {
+        assert!(index.0 < self.indices.len());
+        let (i, j) = index;
+        if j < self.windows[i].0 || j >= self.windows[i].1 {
+            return &f32::INFINITY;
+        }
+        let row_start = self.indices[i];
+        &self.cost_matrix[row_start + j - self.windows[i].0]
+    }
 }
 
-pub(crate) struct DTWExact;
+pub(crate) trait Sample: Sized {
+    fn mean(&self, other: &Self) -> Self;
 
-impl DTWExact {
-    pub(crate) fn best_path(
-        &self,
-        accumulated_cost_matrix: &Array2<f32>,
-    ) -> std::vec::Vec<(usize, usize)> {
-        let (n, m) = accumulated_cost_matrix.dim();
-        let mut i = n - 1;
-        let mut j = m - 1;
-        let mut path = vec![(i, j)];
+    fn dist(&self, other: &Self) -> f32;
+}
 
-        while (i > 0) || (j > 0) {
-            if i == 0 {
-                path.push((0, j - 1));
-                j -= 1;
-            } else if j == 0 {
-                path.push((i - 1, 0));
-                i -= 1;
+impl Sample for [f32; 13] {
+    fn mean(&self, other: &Self) -> Self {
+        let mut ret = [0.; 13];
+        for i in 0..13 {
+            ret[i] = (self[i] + other[i]) / 2.;
+        }
+        ret
+    }
+    fn dist(&self, other: &Self) -> f32 {
+        // Options:
+        // Inner product distance (this)
+        // or
+        // Euclidean distance
+        let norm0 = self[1..].iter().map(|x| x * x).sum::<f32>().sqrt();
+        let norm1 = other[1..].iter().map(|x| x * x).sum::<f32>().sqrt();
+        let dot = self[1..]
+            .iter()
+            .zip(other[1..].iter())
+            .map(|(a, b)| a * b)
+            .sum::<f32>();
+        (1. - dot / (norm0 * norm1)).abs()
+    }
+}
+
+pub(crate) fn dtw<T>(x: &[T], y: &[T], window: Option<Vec<(usize, usize)>>, psi: Psi) -> DtwResult
+where
+    T: Sample,
+{
+    let cost_matrix = cost_matrix(x, y, window, psi);
+    best_path(&cost_matrix, psi)
+}
+
+pub(crate) fn cost<T: Sample>(
+    x: &[T],
+    y: &[T],
+    window: Option<Vec<(usize, usize)>>,
+    psi_: Psi,
+) -> f32 {
+    let cost_matrix = cost_matrix(x, y, window, psi_);
+    let result = psi(&cost_matrix, psi_);
+    result.cost
+}
+
+pub(crate) fn find_subsequence<'a, T: Sample + 'a>(
+    needle: &[T],
+    haystack: impl Iterator<Item = &'a [T]>,
+    psi: usize,
+) -> (usize, f32) {
+    haystack
+        .scan(f32::INFINITY, |ub, series| {
+            println!("{ub}");
+            let new_cost = cost_eapruned(series, needle, *ub, psi).unwrap_or(f32::INFINITY);
+            *ub = ub.min(new_cost);
+            Some(new_cost)
+        })
+        .enumerate()
+        .min_by_key(|(_, cost)| CmpF32::from(*cost))
+        .unwrap()
+}
+
+#[derive(Default, Clone, Copy)]
+pub(crate) struct Psi {
+    pub(crate) a_start: usize,
+    pub(crate) a_end: usize,
+    pub(crate) b_start: usize,
+    pub(crate) b_end: usize,
+}
+
+pub(crate) fn cost_eapruned<T>(mfcc1: &[T], mfcc2: &[T], ub: f32, psi: usize) -> Option<f32>
+where
+    T: Sample,
+{
+    let (co, li) = if mfcc1.len() < mfcc2.len() {
+        (mfcc1, mfcc2)
+    } else {
+        (mfcc2, mfcc1)
+    };
+
+    let mut buffers = vec![f32::INFINITY; (1 + co.len()) * 2];
+    let mut costs = vec![f32::INFINITY; li.len()];
+    let mut c = 1;
+    let mut p = co.len() + 2;
+    buffers[c - 1] = 0.;
+    let mut next_start = 0;
+    let mut pruning_point = 0;
+
+    for i in 0..li.len() {
+        std::mem::swap(&mut c, &mut p);
+
+        let x = &li[i];
+
+        let j_stop = co.len();
+        let j_start = 0;
+        let mut next_pruning_point = j_start;
+        let mut j = j_start;
+        next_start = j_start;
+
+        // Init the first column
+        buffers[c + j - 1] = f32::INFINITY;
+
+        let mut cost = f32::INFINITY;
+
+        // Compute DTW up to the pruning point while advancing next_start: diag and top
+        while j == next_start && j < pruning_point {
+            let d = Sample::dist(x, &co[j]);
+            cost = f32::min(buffers[p + j - 1], buffers[p + j]) + d;
+            buffers[c + j] = cost;
+            if cost <= ub {
+                next_pruning_point = j + 1;
             } else {
-                let moves = [(i - 1, j), (i, j - 1), (i - 1, j - 1)];
-                let move_ = moves
-                    .iter()
-                    .min_by(|first, second| {
-                        let first = accumulated_cost_matrix[**first];
-                        let second = accumulated_cost_matrix[**second];
-                        if first > second {
-                            Ordering::Greater
-                        } else {
-                            Ordering::Less
-                        }
-                    })
-                    .unwrap();
-                path.push(*move_);
-                (i, j) = *move_;
+                next_start += 1;
             }
-        }
-        path.reverse();
-        path
-    }
-    pub(crate) fn accumulated_cost_matrix(&self, cost_matrix: &mut Array2<f32>) {
-        let (n, m) = cost_matrix.dim();
-        for j in 1..m {
-            cost_matrix[(0, j)] += cost_matrix[(0, j - 1)];
+            j += 1;
         }
 
-        for i in 1..n {
-            cost_matrix[(i, 0)] += cost_matrix[(i - 1, 0)];
-            for j in 1..m {
-                cost_matrix[(i, j)] += cost_matrix[(i - 1, j)]
-                    .min(cost_matrix[(i, j - 1)])
-                    .min(cost_matrix[(i - 1, j - 1)]);
+        // Compute DTW up to the pruning point without advancing next_start: prev, diag, top
+        while j < pruning_point {
+            let d = Sample::dist(x, &co[j]);
+            cost = min!(cost, buffers[p + j - 1], buffers[p + j]) + d;
+            buffers[c + j] = cost;
+            if cost <= ub {
+                next_pruning_point = j + 1;
             }
+            j += 1;
+        }
+
+        // Compute DTW at pruning point
+        if j < j_stop {
+            let d = Sample::dist(x, &co[j]);
+            if j == next_start {
+                // Advancing next start: only diag. Done if v>UB.
+                cost = buffers[p + j - 1] + d;
+                buffers[c + j] = cost;
+                if cost <= ub {
+                    next_pruning_point = j + 1;
+                } else {
+                    break;
+                }
+            } else {
+                cost = min!(cost, buffers[p + j - 1]) + d;
+                buffers[c + j] = cost;
+                if cost <= ub {
+                    next_pruning_point = j + 1;
+                }
+            }
+            j += 1;
+        } else if j == next_start {
+            return None;
+        }
+
+        // Compute DTW after pruning point: prev. Go on while we advance the next pruning point
+        while j == next_pruning_point && j < j_stop {
+            let d = Sample::dist(x, &co[j]);
+            cost += d;
+            buffers[c + j] = cost;
+            if cost <= ub {
+                next_pruning_point += 1;
+            }
+            j += 1;
+        }
+        if j == j_stop {
+            costs[i] = cost;
+        }
+        pruning_point = next_pruning_point;
+    }
+
+    costs[costs.len() - psi..].iter().copied().min_by_key(|cost|CmpF32::from(*cost))
+}
+
+pub(crate) fn cost_matrix<T>(
+    mfcc1: &[T],
+    mfcc2: &[T],
+    window: Option<Vec<(usize, usize)>>,
+    psi: Psi,
+) -> SparseCostMatrix
+where
+    T: Sample,
+{
+    let window = window.unwrap_or_else(|| {
+        (0..mfcc1.len())
+            .map(|_| (0, mfcc2.len()))
+            .collect::<Vec<_>>()
+    });
+
+    let indices: Vec<usize> = window
+        .iter()
+        .scan(0, |acc, win| {
+            let ret = *acc;
+            *acc += win.1 - win.0;
+            Some(ret)
+        })
+        .collect();
+
+    let len_cost_matrix = *indices.last().unwrap();
+    let mut cost_matrix = SparseCostMatrix {
+        indices,
+        cost_matrix: Vec::with_capacity(len_cost_matrix),
+        windows: window.clone(),
+        size: (mfcc1.len(), mfcc2.len()),
+    };
+
+    for ((range_start, range_end), (i, row)) in window.iter().zip(mfcc1.iter().enumerate()) {
+        for (offset, column) in mfcc2[*range_start..*range_end].iter().enumerate() {
+            let j = range_start + offset;
+            let dist = row.dist(column);
+            let min_prev = match (i, j) {
+                (0, 0) => 0.,
+                (0, j) if j < psi.b_start => dist,
+                (0, _) => cost_matrix[(i, j - 1)],
+                (i, 0) if i < psi.a_start => dist,
+                (_, 0) => cost_matrix[(i - 1, j)],
+                (_, _) => {
+                    min!(
+                        cost_matrix[(i - 1, j)],
+                        cost_matrix[(i, j - 1)],
+                        cost_matrix[(i - 1, j - 1)]
+                    )
+                }
+            };
+            cost_matrix.cost_matrix.push(min_prev + dist);
         }
     }
-    pub(crate) fn cost_matrix(
-        &self,
-        mfcc1: &ArrayView2<f32>,
-        mfcc2: &ArrayView2<f32>,
-    ) -> Array2<f32> {
-        // Discard the first component
-        let mfcc1 = mfcc1.slice(s![.., 1..]);
-        let mfcc2 = mfcc2.slice(s![.., 1..]);
 
-        let normsq_1 = (&mfcc1 * &mfcc1).sum_axis(Axis(1)).map(|elem| elem.sqrt());
-        let normsq_2 = (&mfcc2 * &mfcc2).sum_axis(Axis(1)).map(|elem| elem.sqrt());
+    cost_matrix
+}
 
-        let cost_matrix = &mfcc1.dot(&mfcc2.t());
-        let norm_matrix = &normsq_1 * &normsq_2;
-        1. - (cost_matrix / norm_matrix)
+struct PsiResult {
+    path_end: (usize, usize),
+    cost: f32,
+}
+
+fn psi(accumulated_cost_matrix: &SparseCostMatrix, psi: Psi) -> PsiResult {
+    let (n, m) = accumulated_cost_matrix.size();
+    let i_min = ((n - psi.a_end)..n)
+        .min_by(|&a, &b| {
+            accumulated_cost_matrix[(a, m - 1)].total_cmp(&accumulated_cost_matrix[(b, m - 1)])
+        })
+        .unwrap_or(n - 1);
+
+    let j_min = ((m - psi.b_end)..m)
+        .min_by(|&a, &b| {
+            accumulated_cost_matrix[(m - 1, a)].total_cmp(&accumulated_cost_matrix[(m - 1, b)])
+        })
+        .unwrap_or(m - 1);
+
+    let (i, j) = [(i_min, m - 1), (n - 1, j_min)]
+        .into_iter()
+        .min_by(|&a, &b| accumulated_cost_matrix[a].total_cmp(&accumulated_cost_matrix[b]))
+        .unwrap();
+
+    let cost = accumulated_cost_matrix[(i, j)];
+    PsiResult {
+        path_end: (i, j),
+        cost,
     }
 }
 
-impl Dtw for DTWExact {
-    fn path(&self, mfcc1: &Array2<f32>, mfcc2: &Array2<f32>) -> Vec<(usize, usize)> {
-        let mut cost_matrix = self.cost_matrix(&mfcc1.view(), &mfcc2.view());
-        self.accumulated_cost_matrix(&mut cost_matrix);
-        self.best_path(&cost_matrix)
+fn best_path(accumulated_cost_matrix: &SparseCostMatrix, psi_: Psi) -> DtwResult {
+    let (n, m) = accumulated_cost_matrix.size();
+    let mut path = Vec::with_capacity(n + m);
+
+    let PsiResult {
+        path_end: (mut i, mut j),
+        cost,
+    } = psi(accumulated_cost_matrix, psi_);
+
+    path.push((i, j));
+    loop {
+        // One we hit the "left" or "bottom" of the matrix and we're within the
+        // psi bounds, we're done
+        if (i == 0 && j <= psi_.b_start) || (j == 0 && i <= psi_.a_start) {
+            break;
+        }
+        let min_move = match (i, j) {
+            (0, _) => (i, j - 1),
+            (_, 0) => (i - 1, j),
+            _ => [(i - 1, j), (i, j - 1), (i - 1, j - 1)]
+                .into_iter()
+                .min_by(|&idx0, &idx1| {
+                    accumulated_cost_matrix[idx0].total_cmp(&accumulated_cost_matrix[idx1])
+                })
+                .unwrap(),
+        };
+        path.push(min_move);
+        (i, j) = min_move;
     }
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test_dtw() {
-        let dtw = DTWExact;
-        let arr = arr2(&[[1., 2., 3., 4.]; 4]);
-        let arr = arr.view();
-        // A cost matrix between two identical matrices should be zero
-        let mut cost_matrix = dtw.cost_matrix(&arr, &arr);
-        assert!(cost_matrix.abs_diff_eq(&Array2::zeros(cost_matrix.dim()), 1e-6));
-
-        dtw.accumulated_cost_matrix(&mut cost_matrix);
-        assert!(cost_matrix.abs_diff_eq(&Array2::zeros(cost_matrix.dim()), 1e-6));
-    }
-
-    #[test]
-    fn test_acm() {
-        let dtw = DTWExact;
-        let arr = [
-            [0., 1., 2., 3., 4.],
-            [5., 6., 7., 8., 9.],
-            [10., 11., 12., 13., 14.],
-            [15., 16., 17., 18., 19.],
-        ];
-        let mut cm = arr2(&arr);
-        dtw.accumulated_cost_matrix(&mut cm);
-        assert_eq!(
-            cm,
-            arr2(&[
-                [0., 1., 3., 6., 10.],
-                [5., 6., 8., 11., 15.],
-                [15., 16., 18., 21., 25.],
-                [30., 31., 33., 36., 40.]
-            ])
-        );
-    }
-
-    #[test]
-    fn test_cm() {
-        let dtw = DTWExact;
-        let a = arr2(&[
-            [0.43123915, 0.14245438, 0.05888148, 0.9096692, 0.54578147],
-            [0.36400301, 0.24935615, 0.88831442, 0.63916911, 0.16509406],
-            [0.0823209, 0.29505345, 0.88290994, 0.41310553, 0.03780696],
-            [0.32343827, 0.46915656, 0.44493509, 0.68262621, 0.2717263],
-        ]);
-
-        let b = arr2(&[
-            [
-                0.68057345, 0.95531281, 0.07671825, 0.5245585, 0.45179126, 0.06398397, 0.30966451,
-                0.7299773,
-            ],
-            [
-                0.0821018, 0.00303263, 0.69903472, 0.39051233, 0.11193357, 0.86482746, 0.51732433,
-                0.60862085,
-            ],
-            [
-                0.94818144, 0.71147869, 0.99692792, 0.09728868, 0.06965384, 0.45353979, 0.58017138,
-                0.81655581,
-            ],
-            [
-                0.54442928, 0.91329335, 0.02092644, 0.80129873, 0.09143143, 0.38208321, 0.22788604,
-                0.85899155,
-            ],
-        ]);
-
-        assert!(dtw.cost_matrix(&a.view(), &b.view()).abs_diff_eq(
-            &arr2(&[
-                [
-                    0.47545551, 0.37896973, 0.42917976, 0.0756509, 0.04015874, 0.08138541,
-                    0.22563027, 0.13886885
-                ],
-                [
-                    0.1661137, 0.09157354, 0.35375425, 0.07878056, 0.06302912, 0.17034899,
-                    0.17305567, 0.01729046
-                ],
-                [
-                    0.20928561, 0.32593934, 0.06678934, 0.33771581, 0.05455241, 0.03952407,
-                    0.00321784, 0.07173629
-                ],
-                [
-                    0.27219956, 0.223295, 0.29883809, 0.08719482, 0.00786294, 0.06631786,
-                    0.12345533, 0.03624143
-                ],
-                [
-                    0.4378936, 0.25661218, 0.59277324, 0.0042661, 0.10537615, 0.21447146,
-                    0.34738579, 0.14493613
-                ]
-            ]),
-            1e-6
-        ));
-    }
-
-    #[test]
-    fn test_path() {
-        let dtw = DTWExact;
-        let acm = arr2(&[
-            [0.43123915, 0.14245438, 0.05888148, 0.9096692, 0.54578147],
-            [0.36400301, 0.24935615, 0.88831442, 0.63916911, 0.16509406],
-            [0.0823209, 0.29505345, 0.88290994, 0.41310553, 0.03780696],
-            [0.32343827, 0.46915656, 0.44493509, 0.68262621, 0.2717263],
-        ]);
-
-        assert_eq!(
-            dtw.best_path(&acm),
-            vec![
-                (0, 0),
-                (0, 1),
-                (0, 2),
-                (0, 3),
-                (0, 4),
-                (1, 4),
-                (2, 4),
-                (3, 4)
-            ]
-        )
-    }
+    path.shrink_to_fit();
+    path.reverse();
+    DtwResult { path, cost }
 }
